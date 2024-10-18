85d2a29a17cdf5aea8f39f5603b566524d4fd08e
abhinan6u <nandupostbank@gmail.com>
2024-10-18 20:07:40 +0530

    Describe the changes you made here

---
+++ javascript-learning/script.js
@@ -0,0 +1,7 @@
+// function
+// es5 and es6
+// es5 contain 3 types of fns=> fn statements , fn expressions and anonymous fns
+//es6 contain => fat arrow fn this can be written in 3 types 
+// a) basic fat arrow 
+// b) fat arrow with one parameter
+// c) fat arrow with implicit return
