d5babe5928e57b7407d61d61cda199a7d6e40ca4
abhinan6u <nandupostbank@gmail.com>
2024-10-18 20:17:34 +0530

    some changes have been done

--- javascript-learning/script.js
+++ javascript-learning/script.js
@@ -5,3 +5,16 @@
 // a) basic fat arrow 
 // b) fat arrow with one parameter
 // c) fat arrow with implicit return
+
+// // functions - js mein function normal programming
+// language jaise nahi hai, iska matlab hai, yaha par js
+// mein, functions banaane ke liye type nahi batana padt
+// bas aapko function word likhna padega and aap usey ko
+// naam deke bana skte ho
+
+// // indepth - js mein functions first class functions
+// ka darja diya gaya hai, jiska matlab hai, ki js mein
+// functions ko value/variable ki tarah treat kiya
+
+// what is function => the code which we use repeatedly again and again
+// why we need =>to reuse the code and which we want to use that code again and again in future
