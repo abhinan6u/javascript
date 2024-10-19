1467f90862e5b7bda9e308c67c33b66a734e0944
abhinan6u <nandupostbank@gmail.com>
2024-10-19 23:02:55 +0530

    ok bye,,,for today

---
+++ DOM/script.js
@@ -0,0 +1,54 @@
+// dom 
+// access the element
+
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// const button = document.querySelector("button")
+
+// const frstImg = document.querySelector("#abcd");
+// const secndImg = document.querySelector(".abcd");
+// const frstHeading = document.querySelector("#def");
+// const secndheading = document.querySelector(".def");
+// console.log(frstHeading);
+
+// document.querySelectorAll 
+
+
+// modifying elements
+// 1)textContent
+// 2)innerHtml
+
+
+
+// const button = document.querySelector("button");
+// setTimeout(()=>{
+//     button.textContent = "Starting now ..."
+// },2000);
+
+const h1 =  document.querySelector(".def");
+console.log(h1);
+h1.textContent += "abhinandu"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DOM/script2.js
@@ -0,0 +1,28 @@
+// manupulating style and Classes
+// const h1 = document.querySelector("h1")
+// h1.style.color = "blue";
+// h1.style.fontFamily = "gilroy";
+
+// h1.classList.add("addLIst") = > check this later something new
+
+// // Creating and Deleting Elements
+
+// {
+// var h1 = document.createElement("h1");
+// h1.textContent = "hey nandu";
+// h1.classList.add("makeitred")
+// } ==> creating an element only
+
+// { inserting the element appendChild fn
+
+// const im = document.createElement("img");
+// im.src = "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+// document.querySelector("body").appendChild(im);
+
+// }
+// {delete the element removechild fn}
+
+// eventHandling...{addEvenListener fn}
+
+
+
